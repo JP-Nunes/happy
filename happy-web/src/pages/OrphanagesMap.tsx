9a958fb4fbe87368f217058e5842aa4accc2dee6
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-10-13 10:25:20 -0300

    Initianting frontend with landing and orphanages map pages

---
+++ happy-web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
+
+import mapMarkerImg from '../images/map-marker.svg'
+
+import '../styles/pages/orphanages-map.css'
+
+function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+        
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Osasco</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+
+      <Map
+        center={[-23.535484,-46.7890726]}
+        zoom={14}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+    </div>
+  )
+}
+
+export default OrphanagesMap
