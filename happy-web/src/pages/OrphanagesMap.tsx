9a37855d9f98248dc8646e938939b9fb9aa56a36
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-10-14 15:58:16 -0300

    Implementing Orphanage and CreateOrphanage pages, and abstracting repeated components

--- happy-web/src/pages/OrphanagesMap.tsx
+++ happy-web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FiPlus } from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet'
-
-import 'leaflet/dist/leaflet.css'
+import { FiPlus, FiArrowRight } from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
 import mapMarkerImg from '../images/map-marker.svg'
+import happyMapIcon from '../utils/happyMapIcon'
 
 import '../styles/pages/orphanages-map.css'
+import 'leaflet/dist/leaflet.css'
 
 function OrphanagesMap() {
   return (
@@ -38,8 +38,23 @@ function OrphanagesMap() {
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      
+        <Marker 
+          icon={happyMapIcon}
+          position={[-23.535484,-46.7890726]}
+        >
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+            Lar das meninas
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="FFF" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
+      <Link to="orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
     </div>
   )
 }
