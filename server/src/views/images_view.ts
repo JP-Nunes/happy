5c5c64ec71188ec193d4cbad123b268659ada389
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-10-20 10:30:44 -0300

    configuring environment variables

--- server/src/views/images_view.ts
+++ server/src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `http://192.168.0.7:3333/uploads/${image.path}`,
+      url: `http://${process.env.APP_API_URL}/uploads/${image.path}`,
     }
   },
 
